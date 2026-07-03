4ab648b370b38adf1d5308740b934f40be1015b6
Lazyfox1997 <aseshsinha121@gmail.com>
2026-07-03 17:10:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function(event) {
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      event.preventDefault();
-      target.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
